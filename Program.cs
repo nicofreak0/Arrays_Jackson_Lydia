d3f778d273512f2098169939ecfd8405e027dd96
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-28 13:23:01 -0500

    f

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
+            //asks user to enter a whole number for the array size
             Console.WriteLine("Enter a whole number for the array size.");
             int arraySize = Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[arraySize];
             int sum = 0;
 
+            //asks user to enter another whole number to add to the array,
+            //adds the numbers together to get the sum of the array numbers
             for (int i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine("Enter a whole number to add to the array.");
@@ -16,8 +19,10 @@
                 sum += numbers[i];
             }
 
+            //prints to console the answer of the array sum
             Console.WriteLine($"The sum of the array is {sum}");
 
+            //sorts the array numbers in order and prints to console
             Array.Sort( numbers );
 
             Console.WriteLine("Array:");
