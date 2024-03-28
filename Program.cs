8f6eeeb0c15b3f86a3ba478c2a0bafd831e658ee
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-28 13:09:42 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Arrays_Jackson_Lydia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter a whole number for the array size.");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[arraySize];
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter a whole number to add to the array.");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                sum += numbers[i];
+            }
+
+            Console.WriteLine($"The sum of the array is {sum}");
+
+            Array.Sort( numbers );
+
+            Console.WriteLine("Array:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arrays_Jackson_Lydia
+ 
